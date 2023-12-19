11bdfbf4fc53c854c7bf0c2a457edd7f3bd4f83f
fumi9999 <fumi9999@gmail.com>
2023-12-19 13:47:39 +0900

    Azure db for mysql用　動いていない

--- backend/db_control/connect.py
+++ backend/db_control/connect.py
@@ -3,10 +3,20 @@ import platform
 print(platform.uname())
 
 from sqlalchemy import create_engine
-import os
+import sqlalchemy
 
-main_path = os.path.dirname(os.path.abspath(__file__))
-path = os.chdir(main_path)
-print(path)
+# import os
+# main_path = os.path.dirname(os.path.abspath(__file__))
+# path = os.chdir(main_path)
+# print(path)
+# engine = create_engine("sqlite:///CRM.db", echo=True)
 
-engine = create_engine("sqlite:///CRM.db", echo=True)
+import config
+
+user = config.DB_USER
+port = config.PORT
+password = config.PASSWORD
+host = config.HOST
+db_name = config.DATABASE
+
+engine = sqlalchemy.create_engine(f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{db_name}")
